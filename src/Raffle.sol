ab5091227168492f2d3cf675e103acde6777d2c5
dheeraj <djsays124@gmail.com>
2024-12-02 02:06:54 +0530

    adding raffle

---
+++ src/Raffle.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+/**
+ * @author Dheeraj joshi
+ * @title Lottery Ticket
+ * @dev this contract creating the sample raffle
+ */
+
+contract Raffle {
+    uint256 entranceFee;
+    function enterRaffle()  public () {
+        
+    }
+
+    function PickWinner () public{
+
+    }
+}
