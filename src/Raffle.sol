ee2bb1aa61e8cf56222c092c7df69bdb20167ff0
dheeraj <djsays124@gmail.com>
2024-12-24 01:06:17 +0530

    adding the enum

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -22,11 +22,11 @@
 
 // view & pure functions
 
-
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.19;
 
-import {VRFConsumerBaseV2Plus} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFConsumerBaseV2Plus} from
+    "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 /**
  * @author Dheeraj joshi
@@ -36,59 +36,88 @@ import {VRFV2PlusClient} from "lib/chainlink-brownie-contracts/contracts/src/v0.
 
 contract Raffle is VRFConsumerBaseV2Plus {
     error Raffle__SendMoreEtherRaffle();
-    uint256 private immutable i_entranceFee;
-    // @dev the duration of the lottery in seconds
-    uint256 private immutable i_internal;
+    error Raffle__TransferFailer();
+
+    // Type Declaration
+
+    
+
+    // State variables
+
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     address payable[] private s_players;
+
+    // @dev the duration of the lottery in seconds    
+    uint256 private immutable i_internal;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_entranceFee;
+
     uint256 private s_lastTime;
+    address private s_recentWinner;
 
     // Events
     event RaffleEntered(address indexed player);
 
-
-    constructor (uint256 entranceFee , uint256 internally, address vrfCoordinator) VRFConsumerBaseV2Plus(vrfCoordinator ) {
+    constructor(
+        uint256 entranceFee,
+        uint256 internally,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subScriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
-        i_internal  = internally;
+        i_internal = internally;
         s_lastTime = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subScriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
-    function enterRaffle() external payable {
 
-        if(msg.value < i_entranceFee){
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreEtherRaffle();
             // require(msg.value >= i_entranceFee , SendMoreEtherRaffle());
         }
 
         s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
-        
     }
 
     function PickWinner() external {
-        if((block.timestamp - s_lastTime) < i_internal){
+        if ((block.timestamp - s_lastTime) < i_internal) {
             revert();
         }
 
-                VRFV2PlusClient.RandomWordsRequest request =  VRFV2PlusClient.RandomWordsRequest({
-                keyHash: s_keyHash,
-                subId: s_subscriptionId,
-                requestConfirmations: requestConfirmations,
-                callbackGasLimit: callbackGasLimit,
-                numWords: numWords,
-                extraArgs: VRFV2PlusClient._argsToBytes(
-                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
-                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
-                )
-            })
-            requestId = s_vrfCoordinator.requestRandomWords(
-
-        );
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATION,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override{
-
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        uint256 indexWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexWinner];
+        s_recentWinner = recentWinner;
+        (bool success ,) = recentWinner.call{value : address(this.balance)}("");
+        if(!success){
+            revert Raffle__TransferFailer ();
+        }
     }
 
-    function getEntranceFee () external view returns (uint256){
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
-    } 
+    }
 }
